1f7859bbbee93f5ece1b4c1cad8221211fd239c7
Adam Tang <ya4dang1@gmail.com>
2021-01-12 08:28:53 +0800

    Login with Auth0

---
+++ components/login.js
@@ -0,0 +1,19 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import { Button } from '@material-ui/core';
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      size="large"
+      onClick={() => loginWithRedirect()}
+    >
+      Log In
+    </Button>
+  );
+};
+
+export default LoginButton;
